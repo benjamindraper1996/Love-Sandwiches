bdc2abb0f9ece60d9fed085157fc88211e8f6ba3
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-04-20 21:44:48 +0000

    Collect sales data for last 5 markets in lists of columns

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+"""
+The docstring
+"""
+from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -37,7 +41,7 @@ def get_sales_data():
 
 def validate_data(values):
     """
-    Inside teh try, converts all strings into integers .
+    Inside the try, converts all strings into integers .
     Raises ValueError if strings cannot be converted into int,
     or if there arent exactly 6 values.
     """
@@ -85,6 +89,20 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects colloums of data from sales worksheets, collecting
+    the last 5 entries for each sandwich and returns the data
+     as a list of lists.
+    """
+    sales = SHEET.worksheet('sales')
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 def main():
     """
     Run all program functions.
@@ -97,4 +115,6 @@ def main():
 
 
 print('Welcome to Love Sandwiches Data Automation.')
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
