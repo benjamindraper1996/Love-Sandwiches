f2869696d495418a5e869185eda107c28c0b7b6c
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-04-20 22:15:33 +0000

    add requirements for deployment

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 """
 The docstring
 """
-from pprint import pprint
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -27,7 +26,7 @@ def get_sales_data():
         print('Data should be six numbers, seperated by commas.')
         print('Example: 10,20,30,40,50,60\n')
 
-        data_str = input('Enter your data here: ')
+        data_str = input('Enter your data here:\n')
 
         sales_data = data_str.split(',')
         validate_data(sales_data)
